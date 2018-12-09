190d9ee7d035414637736dd22419e7de37da2b4f
mazaretto <i.dev@mazaretto.pro>
2018-12-09 12:05:43 +0300

    loading all files

---
+++ build/_include/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bootstrap 101 Template</title>
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--	==========================-->
+	<meta name="theme-color" content="#000">
+	<meta name="msapplication-navbutton-color" content="#000">
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+	<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="img/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="img/favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-icon.png">
+	<meta property="og:image" content="img/favicon/favicon-96x96.png">
+	<!--	==========================-->
+	<!--	==========================-->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.min.css">
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+<div class="wrapper">
+	<header id="header">
+		<!-- Fixed navbar -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Меню</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/">M88Themes</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav top-menu">
+						<li><a href="#">Главная</a></li>
+						<li><a href="#">О нас</a></li>
+						<li><a href="#">Контаты</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
+	</header>
+	<div class="content-wrapper">
---
+++ gulpfile.js
@@ -0,0 +1,261 @@
+"use strict";
+//===========================================
+var gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	clean = require('gulp-clean'),
+	cleanCSS = require('gulp-clean-css'),
+	imagemin = require('gulp-imagemin'),
+	imageminPngquant = require('imagemin-pngquant'),
+	pug = require('gulp-pug'),
+	rename = require("gulp-rename"),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	concat = require('gulp-concat'),
+	order = require("gulp-order"),
+	uglify = require('gulp-uglify'),
+	spritesmith = require('gulp.spritesmith'),
+	browserSync = require('browser-sync'),
+	babel = require('gulp-babel'),
+	reload = browserSync.reload,
+	notify = require("gulp-notify"),
+	rigger = require('gulp-rigger');
+//===========================================
+var buidPath = 'build/',
+	path = {
+		build: {
+			home: buidPath,
+			html: buidPath + '**/*.html',
+			htmlHome: buidPath + '*.html',
+			htmlRigger: buidPath + 'html/*.html',
+			htmlRiggerFiles: buidPath + 'html/**/*.html',
+			pug:  buidPath + 'pug/**/*.pug',
+			pugFile:  buidPath + 'pug/*.pug',
+			php:  buidPath + '**/*.php',
+			js:   buidPath + 'js/**/*.js',
+			jsPath: buidPath + 'js/',
+			jsLib: buidPath + 'js/lib/**/*.js',
+			jsLibPath: buidPath + 'js/lib/',
+			sass: buidPath + 'sass/**/*.+(sass|scss)',
+			sassPath: buidPath + 'sass/',
+			css:  buidPath + 'css/**/*.css',
+			cssPath:  buidPath + 'css/',
+			icon: buidPath + 'img/icons/*.+(png|jpg)',
+			img:  buidPath + 'img/**/*.*',
+			imgPath:  buidPath + 'img/',
+			image:  buidPath + 'image/**/*.*',
+			imagePath:  buidPath + 'image/',
+			fonts:  buidPath + 'fonts/**/*.*'
+		},
+		dist: {
+			home: 'dist/',
+			js: 'dist/js/',
+			css: 'dist/css/',
+			img: 'dist/img/',
+			image: 'dist/image/',
+			fonts: 'dist/fonts/'
+		}
+	};
+// Server
+//===========================================
+gulp.task('openServer', function () {
+	browserSync({
+		proxy: 'js.loc/', // local server
+		online: true,
+		host: 'localhost',
+    port: 9000,
+		logPrefix: "openServer"
+	});
+});
+//===========================================
+gulp.task('webServer', function () {
+	browserSync({
+		server: {
+			baseDir: "./build/"
+		},
+		online: true,
+		host: 'localhost',
+		port: 9000,
+		logPrefix: "webServer"
+	});
+});
+// HTML/PHP
+// ===========================================
+gulp.task('html:build', function () {
+	gulp.src(path.build.htmlRigger)
+		.pipe(rigger())
+		.pipe(gulp.dest(path.build.home))
+		.pipe(reload({stream: true}));
+});
+// PUG
+// ===========================================
+gulp.task('pug:build', function () {
+	return gulp.src(path.build.pugFile)
+		.pipe(pug().on("error", notify.onError()))
+		.pipe(gulp.dest(path.build.home))
+		.pipe(reload({stream: true}));
+});
+// JavaScript
+// ===========================================
+gulp.task('js:build', function(){
+	return gulp.src(path.build.jsLib)
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(sourcemaps.init())
+    .pipe(gulp.dest(path.build.jsPath))
+		.pipe(uglify())
+		.pipe(rename(function (path) {
+			path.extname = ".min.js"
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.build.jsPath))
+		.on('error', function(err) {
+			console.error('Error in compress task', err.toString());
+		});
+});
+gulp.task('jsMain:build', function(){
+	return gulp.src(path.build.jsPath + 'main.js')
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(uglify())
+		.pipe(rename(function (path) {
+			path.extname = ".min.js"
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.build.jsPath))
+		.on('error', function(err) {
+			console.error('Error in compress task', err.toString());
+		});
+});
+// SASS/SCSS
+// ===========================================
+gulp.task('sass:build', function () {
+	return gulp.src(path.build.sass)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on("error", notify.onError()))
+		.pipe(autoprefixer(['last 20 versions', '> 1%', 'ie > 8']))
+		.pipe(rename(function (path) {
+			path.extname = ".css"
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.build.cssPath))
+		.pipe(cleanCSS())
+		.pipe(sourcemaps.write())
+		.pipe(rename(function (path) {
+			path.extname = ".min.css"
+		}))
+		.pipe(gulp.dest(path.build.cssPath))
+		.pipe(reload({stream: true}));
+});
+// Img min / sprite
+// ===========================================
+gulp.task('sprite:build', function () {
+	var spriteData = gulp.src(path.build.icon)
+		.pipe(spritesmith({
+			imgName: 'sprite.png',
+			cssName: '_sprite.scss',
+			algorithm: 'diagonal',
+			// padding: 100,
+			imgPath: '../img/sprite.png'
+		}))
+	spriteData.img.pipe(gulp.dest(path.build.imgPath));
+	spriteData.css.pipe(gulp.dest(path.build.sassPath));
+});
+// Clean
+//===========================================
+gulp.task('clean:dist', function () {
+	return gulp.src(path.dist.home, {read: false})
+		.pipe(clean());
+});
+// Dist
+// //===========================================
+gulp.task('dist', ['clean:dist'], function () {
+	gulp.src(path.build.css)
+		.pipe(gulp.dest(path.dist.css))
+	gulp.src(path.build.fonts)
+		.pipe(gulp.dest(path.dist.fonts))
+	gulp.src(path.build.js)
+		.pipe(gulp.dest(path.dist.js))
+	gulp.src(path.build.html)
+		.pipe(gulp.dest(path.dist.home))
+	gulp.src(path.build.php)
+		.pipe(gulp.dest(path.dist.home));
+	gulp.src(path.build.img)
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [imageminPngquant()],
+			interlaced: true
+		}))
+		.pipe(gulp.dest(path.dist.img))
+	gulp.src(path.build.image)
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [imageminPngquant()],
+			interlaced: true
+		}))
+		.pipe(gulp.dest(path.dist.image))
+});
+// Watch
+// ===========================================
+gulp.task('watch', function () {
+	gulp.watch(path.build.htmlHome).on('change', browserSync.reload);
+  gulp.watch(path.build.fonts).on('change', browserSync.reload);
+  gulp.watch(path.build.icon, ['sprite:build']);
+  gulp.watch(path.build.img).on('change', browserSync.reload);
+  gulp.watch(path.build.image).on('change', browserSync.reload);
+  gulp.watch(path.build.sass, ['sass:build']);
+	gulp.watch(path.build.jsLib, ['js:build']);
+  gulp.watch(path.build.jsPath + 'main.js', ['jsMain:build']);
+  gulp.watch(path.build.js).on('change', browserSync.reload);
+});
+// ===========================================
+gulp.task('watchHtml', function () {
+  gulp.watch(path.build.htmlRiggerFiles, ['html:build']);
+  gulp.watch(path.build.fonts).on('change', browserSync.reload);
+  gulp.watch(path.build.icon, ['sprite:build']);
+  gulp.watch(path.build.img).on('change', browserSync.reload);
+  gulp.watch(path.build.image).on('change', browserSync.reload);
+  gulp.watch(path.build.sass, ['sass:build']);
+	gulp.watch(path.build.jsLib, ['js:build']);
+  gulp.watch(path.build.jsPath + 'main.js', ['jsMain:build']);
+  gulp.watch(path.build.js).on('change', browserSync.reload);
+});
+// ===========================================
+gulp.task('watchPug', function () {
+	gulp.watch(path.build.pug, ['pug:build']);
+  gulp.watch(path.build.fonts).on('change', browserSync.reload);
+  gulp.watch(path.build.icon, ['sprite:build']);
+  gulp.watch(path.build.img).on('change', browserSync.reload);
+  gulp.watch(path.build.image).on('change', browserSync.reload);
+  gulp.watch(path.build.sass, ['sass:build']);
+  gulp.watch(path.build.jsLib, ['js:build']);
+  gulp.watch(path.build.jsPath + 'main.js', ['jsMain:build']);
+  gulp.watch(path.build.js).on('change', browserSync.reload);
+});
+// ===========================================
+gulp.task('watchPhp', function () {
+  gulp.watch(path.build.php).on('change', browserSync.reload);
+  gulp.watch(path.build.fonts).on('change', browserSync.reload);
+  gulp.watch(path.build.icon, ['sprite:build']);
+  gulp.watch(path.build.img).on('change', browserSync.reload);
+  gulp.watch(path.build.image).on('change', browserSync.reload);
+  gulp.watch(path.build.sass, ['sass:build']);
+  gulp.watch(path.build.jsLib, ['js:build']);
+  gulp.watch(path.build.jsPath + 'main.js', ['jsMain:build']);
+  gulp.watch(path.build.js).on('change', browserSync.reload);
+});
+
+// Gulp task default
+//===========================================
+gulp.task('default', ['sprite:build','sass:build','js:build','jsMain:build','webServer','watch']);
+//===========================================
+gulp.task('html', ['html:build','sprite:build','sass:build','js:build','jsMain:build','webServer','watchHtml']);
+//===========================================
+gulp.task('pug', ['pug:build','sprite:build','sass:build','js:build','jsMain:build','webServer','watchPug']);
+//===========================================
+gulp.task('php', ['sprite:build','sass:build','js:build','jsMain:build','openServer','watchPhp']); // only with php server
+//===========================================
